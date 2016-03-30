4e9febdc65e92875b03228e0e3009223ddc46d49
Al Kemner <al.kemner@gmail.com>
2016-03-30 19:02:10 -0400

    more stuff

--- script/serverspec.sh
+++ script/serverspec.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo gem install bundler --no-ri --no-rdoc
+sudo chmod 444 /tmp/serverspec/node.json
+sudo su -
+export PATH="/sbin:/usr/local/sbin:/usr/sbin:/opt/chef/embedded/bin:$PATH"
+
 cd /tmp/test
 bundle install --path=vendor
 bundle exec rake spec

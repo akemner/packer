b2f29e923e260403283d9099b49c273d7e79b96a
Al Kemner <al.kemner@gmail.com>
2016-04-02 16:24:41 -0400

    do not remove perl

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -21,5 +21,5 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     fi
 
     echo "==> Removing packages needed for building guest tools"
-    yum -y remove gcc cpp libmpc mpfr kernel-devel kernel-headers perl
+    yum -y remove gcc cpp libmpc mpfr kernel-devel kernel-headers
 fi

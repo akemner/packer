5c1fcdcc68f20cadb1ceef5509d57e3b15c2bc3e
Al Kemner <al.kemner@gmail.com>
2016-04-20 16:08:23 -0400

    lock root on centos6 in kickstart during post. add removing ssh_host_keys and remove cloud-init if it ran

--- script/cloud-cleanup.sh
+++ script/cloud-cleanup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -eux
 
+# find and remove any authorized_keys
 find / -name "authorized_keys" | xargs -i rm -f {}
+
+# wipe cloud-init configuration if it already ran
+rm -rf /var/lib/cloud/instances
+
+# remove ssh_host_keys
+rm /etc/ssh/ssh_host_key
+rm /etc/ssh/ssh_host_rsa_key*
+rm /etc/ssh/ssh_host_dsa_key*
+rm /etc/ssh/ssh_host_ecdsa_key*
